6849bc0f572d4e152a289b4b5935a17776654be0
SANA SHAHIN <40853082+sanashahin2225@users.noreply.github.com>
2022-01-21 20:55:21 +0530

    Different Approach

--- are-anagrams.py
+++ are-anagrams.py
@@ -1,3 +1,4 @@
+#Method-1
 def are_anagrams(s1,s2):
     if len(s1) != len(s2):
         return False
@@ -19,6 +20,21 @@ def are_anagrams(s1,s2):
     
     else:
         return False
+ 
+
+#method-2
+from collections import Counter
+def are_anagrams_with_counter(s1,s2):
+    if len(s1) != len(s2):
+        return False
+    return Counter(s1)==Counter(s2)
+
+
+#Method-3
+def are_anagrams_with_sort(s1,s2):
+    if len(s1) != len(s2):
+        return False
+    return sorted(s1)==sorted(s2)
   
 print(are_anagrams("nameless","salesmen"))  #True
 print(are_anagrams("danger","ganred"))    #True
